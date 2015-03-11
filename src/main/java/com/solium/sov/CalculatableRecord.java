93e240664e1e3a73ec4197f82205978ee9fa0b0d
Polad Mirzayev <polad.mirzayev@gmail.com>
2015-03-11 12:36:33 -0600

    Removed CalculatableRecord interface since it's not used anymore

--- src/main/java/com/solium/sov/CalculatableRecord.java
+++
@@ -1,7 +0,0 @@
-package com.solium.sov;
-
-import java.util.Date;
-
-public interface CalculatableRecord {
-    public double calculateGainFor(Date marketDate, double marketPrice);
-}
--- src/main/java/com/solium/sov/VestRecord.java
+++ src/main/java/com/solium/sov/VestRecord.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-public class VestRecord implements CalculatableRecord, EmployeeAware {
+public class VestRecord implements EmployeeAware {
     private String employeeId;
     private Date vestDate;
     private int numOfUnits;
